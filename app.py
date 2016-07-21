350fb05a9e8fff88b3adcaff62e35ef5477abb4d
Robert Rees <shudderfix@gmail.com>
2016-07-21 23:28:36 +0100

    Adds the Flask SSLify module to see if it fixes on HTTPS

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
+import os
+
 from flask import Flask
+from flask_sslify import SSLify
 
 app = Flask(__name__)
+sslify = SSLify(app)
+
+if os.environ.get('FLASK_ENV', "production") == "development":
+    print "In development mode"
+    app.config['DEBUG'] = True
+
+    print app.config['DEBUG']
 
 @app.route("/")
 def index():
